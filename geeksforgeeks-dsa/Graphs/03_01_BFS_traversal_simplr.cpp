668ba76d9a01d1ca827f33ecc5a12102a3a7a448
subhrajyoti sankar sau <subhrajyotisankarsau@gmail.com>
2025-01-16 23:52:04 +0530

    added graph datastructure

--- geeksforgeeks-dsa/Graphs/03_01_BFS_traversal_simplr.cpp
+++ geeksforgeeks-dsa/Graphs/03_01_BFS_traversal_simplr.cpp
@@ -1,16 +1,26 @@
 class solution {
+    //breadth first search -> level order traversal
     public:
+    //V -> numberOfNodes
     vector<int>bfsOfGraph(int V , vector<int> adj[]){
-        int visited[n] = {0};
+        //every node on the starting is unvisited
+        int visited[V] = {0};
+
+        //starting node
         visited[0]=1;
+
+
         queue<int>q;
         q.push(0);
         vector<int>bfs;
         while(!q.empty()){
             int node = q.front();
             q.pop();
-            bfs.push(node);
+            //push the node value to the bfs vector to store it
+            bfs.push_back(node);
 
+            //searching on the breadth whichever nodes are connected and 
+            //not visited pushed into the queue and the further into the bfs vector
             for(auto it: adj[node]){
                 if(!visited[it]){
                     visited[it]=1;
